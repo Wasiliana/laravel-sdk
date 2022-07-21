4e83ba186194149a91e77718dd06d159a3ab818d
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-07-21 18:23:44 +0300

    improved the function and instantiating the Client and added try catch statement

--- src/Traits/HttpClient.php
+++ src/Traits/HttpClient.php
@@ -2,6 +2,11 @@
 
 namespace Wasiliana\LaravelSdk\Traits;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Exception\TooManyRedirectsException;
 use Wasiliana\LaravelSdk\LaravelSdk;
 
 trait HttpClient
@@ -16,6 +21,31 @@ trait HttpClient
 
     private function postRequest($endpoint, $payload)
     {
-        return $this->sdk->client()->request('POST', $endpoint, ['json' => $payload]);
+        $client = new Client([
+            'base_uri' => 'https://api.wasiliana.com/api/v1/developer/',
+            'headers' => [
+                'apiKey' => config('laravel-sdk.api.key'),
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+
+        try {
+            $response = $client->request('POST', $endpoint, ['json' => $payload]);
+
+            if ((int)$response->getStatusCode() != 200) {
+                return $response->getBody()->getContents();
+            }
+
+            return $response->getBody()->getContents();
+        } catch (ClientException $clientexception) {
+            return $clientexception->getMessage();
+        } catch (ServerException $serverexception) {
+            return $serverexception->getMessage();
+        } catch (ConnectException $connectexception) {
+            return $connectexception->getMessage();
+        } catch (TooManyRedirectsException $toomanyredirectexception) {
+            return $toomanyredirectexception->getMessage();
+        }
     }
 }
