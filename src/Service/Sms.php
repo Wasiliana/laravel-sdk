0b12a24bcc2de727edbc4506e6f844fb5632a0fd
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-07-21 18:25:49 +0300

    added payload, request and send functions.

--- src/Service/Sms.php
+++ src/Service/Sms.php
@@ -2,28 +2,54 @@
 
 namespace Wasiliana\LaravelSdk\Service;
 
+use Wasiliana\LaravelSdk\Traits\ConversationId;
 use Wasiliana\LaravelSdk\Traits\HttpClient;
 
 class Sms
 {
 
-    use HttpClient;
+    use HttpClient, ConversationId;
 
-    public function send($recipients, $message)
+    public function __construct()
     {
     }
 
-    private function validate($recipients)
+    private function checkRecipients($recipients)
     {
         $type = gettype($recipients);
 
         if ($type === 'string') {
+            return [$recipients];
         } elseif ($type === 'array') {
+            return $recipients;
         } else {
+            return null;
         }
     }
 
-    private function bundle()
+    private function payload($recipients, $message)
     {
+        return [
+            'from' => config('laravel-sdk.sms.from'),
+            'message_uid' => $this->uniqueId('outbox'),
+            'recipients' => $recipients,
+            'message' => $message
+        ];
+    }
+
+    private function request($payload)
+    {
+        return $this->postRequest('sms/bulk/send/sms/request', $payload);
+    }
+
+    public function send($recipients, string $message)
+    {
+        $data = $this->checkRecipients($recipients);
+
+        if ($data  == null) return 'Invalid recipients data.';
+
+        $payload = $this->payload($data, $message);
+
+        return $this->request($payload);
     }
 }
