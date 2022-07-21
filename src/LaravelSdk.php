f440a04fb57bf224ab73494bfc0016fff6b74bb2
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-07-21 06:23:33 +0300

    Update LaravelSdk.php

--- src/LaravelSdk.php
+++ src/LaravelSdk.php
@@ -2,7 +2,20 @@
 
 namespace Wasiliana\LaravelSdk;
 
+use GuzzleHttp\ClientInterface;
+
 class LaravelSdk
 {
-    // Build wonderful things
+
+    private $client;
+
+    public function __construct(ClientInterface $clientInterface)
+    {
+        $this->client = $clientInterface;
+    }
+
+    public function client()
+    {
+        return $this->client;
+    }
 }
