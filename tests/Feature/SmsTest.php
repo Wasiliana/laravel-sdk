f5667c9654b17e5c1585761ac2cfaa96f1624051
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-07-28 10:45:11 +0300

    Updated number with a dummy and deleted commented code

--- tests/Feature/SmsTest.php
+++ tests/Feature/SmsTest.php
@@ -9,11 +9,10 @@ class SmsTest extends TestCase
 {
     public function testSend()
     {
-        $response = Sms::recipients('254723384144')
+        $response = Sms::recipients('254723XXXXXX')
             ->message('This cold...Mayoooo!!!')
             ->prefix('test')
             ->send();
-        // $response = Sms::send(['254723384144'],'Testing', 'test');
 
         $this->assertSame($response, ['status' => 'success', 'data' => 'Successfully Dispatched the sms to process'], $strict = false);
     }
