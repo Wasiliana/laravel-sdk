6997ea3a82068fe314fad36418f6f666388e853a
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-08-30 11:30:39 +0300

    added validation of sms payload

--- src/Traits/Validates.php
+++ src/Traits/Validates.php
@@ -6,10 +6,38 @@ use Illuminate\Support\Facades\Validator;
 
 trait Validates
 {
-    protected  function  validateAirtime(array $payload)
+    protected function validateSms(array $data)
     {
         return Validator::make(
-            $payload,
+            $data,
+            [
+                'recipients' => [
+                    function ($attribute, $value, $fail) {
+                        if (!is_string($value) && !is_array($value)) {
+                            $fail(':attribute data type is invalid.');
+                        }
+                    },
+                ],
+                'message' => 'required',
+                'service'  => 'required|array|min:2',
+                'service.name'  => 'required',
+                'service.from'  => 'required',
+                'service.key'  => 'required',
+                'prefix'  => 'required',
+                'isOtp'  => 'required|boolean',
+            ],
+            [
+                'service.min' => 'The service array must have at least 2 items.',
+                'service.name.required' => 'Service name is required.',
+                'service.from.required' => 'Service "from" value is required.',
+                'service.key.required' => 'Api key is required.'
+            ]
+        );
+    }
+    protected function validateAirtime(array $data)
+    {
+        return Validator::make(
+            $data,
             [
                 'service'  => 'required|array',
                 'service.name'  => 'required',
