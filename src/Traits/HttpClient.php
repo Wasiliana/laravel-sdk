80126e8ea55d1cc085a87ac472a047fb9840aeca
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-08-31 09:24:06 +0300

    added a duplicate function, "makeRequest2", which defines a new instance of Client with a different base_uri

--- src/Traits/HttpClient.php
+++ src/Traits/HttpClient.php
@@ -2,6 +2,7 @@
 
 namespace Wasiliana\LaravelSdk\Traits;
 
+use GuzzleHttp\Client;
 use Wasiliana\LaravelSdk\Http\CoreClient;
 
 trait HttpClient
@@ -18,14 +19,32 @@ trait HttpClient
 
     private function makeRequest($endpoint, $payload, array $queryParams = [], string $method = 'POST')
     {
-        return $this->coreClient->client()->request($method, $endpoint, [
-            'headers' => [
-                'apiKey' => $payload['key'],
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-            ],
-            'json' => $payload,
-            'query' => $queryParams
-        ]);
+        return $this->coreClient
+            ->client()
+            ->request($method, $endpoint, [
+                'headers' => [
+                    'apiKey' => $payload['key'],
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => $payload,
+                'query' => $queryParams
+            ]);
+    }
+
+    private function makeRequest2($endpoint, $payload, array $queryParams = [], string $method = 'POST')
+    {
+        return $this->coreClient
+            ->useClient(new Client(['base_uri' => 'https://apiv2.wasiliana.io/api/v1/']))
+            ->client()
+            ->request($method, $endpoint, [
+                'headers' => [
+                    'apiKey' => $payload['key'],
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => $payload,
+                'query' => $queryParams
+            ]);
     }
 }
