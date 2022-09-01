d60872ecae575bf714a5db44f4c97e50a5dbee15
Lawrence Macharia <macharialawrence2011@gmail.com>
2022-09-01 07:30:53 +0300

    updated the name

--- config/wasiliana.php
+++ config/wasiliana.php
@@ -3,7 +3,7 @@
 return [
     'sms' => [
         'service_1' => [
-            'name' => 'test',
+            'name' => 'testSms',
             'from' => env('SMS_SERVICE_1_SENDER_ID', 'WASILIANA'),
             'key' => env('SMS_SERVICE_1_API_KEY', null)
         ],
